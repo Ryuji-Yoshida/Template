cb510b95b9c69f347506b6f903078087ee63caca
yoshida_ryuji <yoshi.ryu1978@gmail.com>
2026-09-11 11:25:55 +0900

    微調整

--- src/assets/js/_module/constants.js
+++
@@ -1,9 +0,0 @@
-// 共通定数・セレクタ
-export const doc = document;
-export const win = window;
-export const htmlTag = 'html';
-export const bodyTag = 'body';
-export const click = 'click';
-export const active = 'is-active';
-export const disable = 'is-disable';
-export const anime = 'is-anime';
